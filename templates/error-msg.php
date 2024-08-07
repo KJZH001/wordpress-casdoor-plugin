019b120dc29f527a4de3ae2207515e48f83a9a5d
KJZH001 <2802007619@qq.com>
2024-08-07 04:09:39 +0000

    汉化 错误信息

--- templates/error-msg.php
+++ templates/error-msg.php
@@ -6,15 +6,15 @@ defined('ABSPATH') or die('No script kiddies please!');
 $message = $wp_query->get('message');
 $alert_message = '';
 if ($message == 'casdoor_login_only') {
-    $alert_message = 'This casdoor account doesn\'t exists in wordpress, please use another.';
+    $alert_message = '您的通行证账户在此站点没有绑定的账户，且此站点关闭了新用户登录，请尝试使用其它账户登录';
 } elseif ($message == 'casdoor_sso_failed') {
-    $alert_message = 'Casdoor Single Sign On Failed. User mismatch or clash with existing data and SSO can not complete.';
+    $alert_message = '通行证单点登录失败！您的用户与现有数据不匹配或冲突，单点登录无法完成。';
 } elseif ($message == 'casdoor_id_not_allowed') {
-    $alert_message = 'For security reasons, this user can not use Single Sign On.';
+    $alert_message = '出于安全原因，您的通行证账户无法完成单点登录';
 }
 
 if (!empty($alert_message)) : ?>
     <div class="error">
-        <p class="alertbar"><?= $alert_message . ' <a href="' . site_url('?auth=casdoor') . '">Please try again</a>'?></p>
+        <p class="alertbar"><?= $alert_message . ' <a href="' . site_url('?auth=casdoor') . '">请重试</a>'?></p>
     </div>
 <?php endif; ?>
